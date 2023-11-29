d68f6b9762b17815a41baefa9463ed20d2a71ec5
Daniel Granada <j.daniel_granada@hotmail.com>
2023-11-29 11:29:14 -0500

    checking

--- js/script.js
+++ js/script.js
@@ -34,3 +34,36 @@ function monsterChosenByTheComputer() {
     var chosenMosterBtyPC = document.getElementById("enemy-monster-name");
     chosenMosterBtyPC.innerHTML = monsters[Math.floor(Math.random() * monsters.length)];
 }
+
+let playerMonsterAttack 
+
+function attack1() {
+    playerMonsterAttack = 'attack-1';
+    alert(playerMonsterAttack);
+}
+
+function attack2() {
+    playerMonsterAttack = 'attack-2';
+    alert(playerMonsterAttack);
+}
+
+function attack3() {
+    playerMonsterAttack = 'attack-3';
+    alert(playerMonsterAttack);
+}
+
+function attack4() {
+    playerMonsterAttack = 'attack-4';
+    alert(playerMonsterAttack);
+}
+
+function playerAttack() {
+    let buttonAttack1 = document.getElementById("attack-1");
+    buttonAttack1.addEventListener("click", attack1)
+    let buttonAttack2 = document.getElementById("attack-2");
+    buttonAttack2.addEventListener("click", attack2)
+    let buttonAttack3 = document.getElementById("attack-3");
+    buttonAttack3.addEventListener("click", attack3)
+    let buttonAttack4 = document.getElementById("attack-4");
+    buttonAttack4.addEventListener("click", attack4)
+}
